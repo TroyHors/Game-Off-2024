de5d3001bf4bb3dad5eadd858c7ff45c1985f3a3
TroyHors <erictroyhors@gmail.com>
2024-11-20 00:17:27 -0500

    Temp Bag

--- Assets/Scripts/Core/Inventory/InventoryManager.cs
+++ Assets/Scripts/Core/Inventory/InventoryManager.cs
@@ -2,16 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InventoryManager : Singleton<InventoryManager> {
+    public class DragData {
+        public SlotHolder originalHolder;
+        public RectTransform originalParent;
+    }
     // TODO: 最后添加模版用于保存数据
     [Header( "Inventory Data" )]
     public InventoryData_SO inventoryData;
     [Header( "Containers" )]
     public ContainerUI inventoryUI;
-
+    [Header( "Drag Canvas" )]
+    public Canvas dragCanvas;
+    public DragData currentDrag;
     void Start() {
         inventoryUI.RefreshUI();
+    }
+    public bool CheckInInventoryUI( Vector3 position ) {
+        for (int i = 0 ; i < inventoryUI.slotHolders.Length ; i++) {
+            RectTransform t = inventoryUI.slotHolders[ i ].transform as RectTransform;
+
+            if (RectTransformUtility.RectangleContainsScreenPoint( t , position )) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
